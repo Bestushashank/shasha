6aa008e753fd27e28a24260bc8494c54cfbc09df
Bestushashank <109731653+Bestushashank@users.noreply.github.com>
2022-08-27 06:45:47 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,7 +1,7 @@
 class login:
     def __init__(self,id,pas):
         self.id="admin"
-        self.thtrtrhtrthas="admin"
+        self.thtrtrhtrthajjjjjs="admin"
 
     def check(id,pas):
         print self.id
